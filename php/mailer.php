db2a6d00803b5e75d5125a8079ce761684bf6470
Anum Asif <anum@cockar.com>
2018-12-18 15:07:28 +0300

    Remove errors from php file

--- php/mailer.php
+++ php/mailer.php
@@ -10,11 +10,11 @@
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)){
       //set a 400 (bad request) response code and exit.
       http_response_code(400);
-      echo "Oops! There was a problem with your submission. Please complete the form and try again."
+      echo "Oops! There was a problem with your submission. Please complete the form and try again.";
       exit;
     }
     //set the recipient email address.
-    //FIXME:update this to your desired email address.
+   
     $recipient = "anum@cockar.com";
     //Set the email subject.
     $subject = "New contact from $name";
@@ -26,7 +26,7 @@
     $email_headers = "Form:$name<$email>";
 
     //Send the Email
-    if(mail($receipient, $sunject, $email_content, $email_headers))_
+    if(mail($recipient, $subject, $email_content, $email_headers))
     //set a 200(okay) response code.
     http_response_code(200);
     echo "Thankyou! Your message has been sent.";
